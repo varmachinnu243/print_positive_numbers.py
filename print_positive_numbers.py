55fd72c44dbf024aa9c1eaeddccce421a01c4815
varmachinnu243 <85660367+varmachinnu243@users.noreply.github.com>
2021-06-28 20:34:00 +0530

    Create print_positive_numbers.py

---
+++ print_positive_numbers.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,-14]
+list2=[12,-95,14,3]
+print("Positive numbers in list1 are: ")
+for i in list1:
+	if i>0:
+		print(i)
+
+print("Positive numbers in list2 are:")
+for j in list2:
+	if j>0:
+		print(j)
